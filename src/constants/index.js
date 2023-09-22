9601760a55ec78a5b830151b8131774da89979b4
Kkura02 <genofin02@gmail.com>
2023-09-22 16:46:32 +0800

    imported the real images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,12 @@
-import { kirara, ningguang, raiden, regular1 } from '../assets/Images'
+import { sfw01, sfw02, sfw03, sfw04, sfw05, sfw06, sfw07, sfw08, sfw09, sfw10, sfw11, sfw12, sfw13, sfw14, sfw15, sfw16, sfw17, sfw18, sfw19, sfw20, sfw21, sfw22, sfw23, sfw24, sfw25, sfw26, sfw27, sfw28, sfw29, sfw30, sfw31, sfw32, sfw33, sfw34, sfw35, sfw36, sfw37, sfw38, sfw39, sfw40, sfw41, sfw42, sfw43, sfw44, sfw45, sfw46, sfw47, sfw48, sfw49, sfw50, sfw51, sfw52, merch01, merch02, merch03, merch04, nsfw01, nsfw02, nsfw03, nsfw04, nsfw05, nsfw06, nsfw07, nsfw08, nsfw09, nsfw10, nsfw11, nsfw12, nsfw13, nsfw14, nsfw15, nsfw16, nsfw17, nsfw18, nsfw19, nsfw20, nsfw21, nsfw22, nsfw23, nsfw24, nsfw25, nsfw26, nsfw27, nsfw28 } from '../assets/Images'
 
-const featuredImages = [kirara, ningguang, raiden, regular1]
+const featuredImages = [sfw10, sfw06, sfw46, sfw20, sfw33, sfw06]
 
 export const gallery = [
     {
-        SFW: [regular1, kirara, ningguang, raiden, regular1, kirara, ningguang, raiden, regular1, kirara, ningguang, raiden, regular1, kirara, ningguang, raiden,],
-        MERCH: [regular1, regular1, regular1, regular1, raiden,],
-        NSFW: [raiden, regular1, kirara, kirara, kirara]
+        SFW: [sfw01, sfw02, sfw03, sfw04, sfw05, sfw06, sfw07, sfw08, sfw09, sfw10, sfw11, sfw12, sfw13, sfw14, sfw15, sfw16, sfw17, sfw18, sfw19, sfw20, sfw21, sfw22, sfw23, sfw24, sfw25, sfw26, sfw27, sfw28, sfw29, sfw30, sfw31, sfw32, sfw33, sfw34, sfw35, sfw36, sfw37, sfw38, sfw39, sfw40, sfw41, sfw42, sfw43, sfw44, sfw45, sfw46, sfw47, sfw48, sfw49, sfw50, sfw51],
+        MERCH: [merch01, merch02, merch03, merch04],
+        NSFW: [nsfw01, nsfw02, nsfw03, nsfw04, nsfw05, nsfw06, nsfw07, nsfw08, nsfw09, nsfw10, nsfw11, nsfw12, nsfw13, nsfw14, nsfw15, nsfw16, nsfw17, nsfw18, nsfw19, nsfw20, nsfw21, nsfw22, nsfw23, nsfw24, nsfw25, nsfw26, nsfw27, nsfw28]
     }
 ]
 
